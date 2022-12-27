159a97281c5c69c243b09d08b1d88c27f6fa98f1
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2022-12-27 13:15:48 +0900

    sampleにmainを追加

---
+++ sample/main.cpp
@@ -0,0 +1,16 @@
+#include"command_manager.hpp"
+#include"create_view.hpp"
+#include"descriptor_heap.hpp"
+#include"device.hpp"
+#include"pipeline_state.hpp"
+#include"resource.hpp"
+#include"root_signature.hpp"
+#include"shader.hpp"
+#include"swap_chain.hpp"
+#include"utility.hpp"
+#include"window.hpp"
+
+int main()
+{
+
+}
