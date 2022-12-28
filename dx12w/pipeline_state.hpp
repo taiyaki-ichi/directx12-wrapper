acf998e474af2a9dda85444ab687f88bdb719ca5
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2022-12-28 15:00:49 +0900

    セミコロン忘れ

--- dx12w/pipeline_state.hpp
+++ dx12w/pipeline_state.hpp
@@ -20,7 +20,7 @@ namespace dx12w
 	// 深度をとるだけとかのrenderTargerFormatsが空の場合はshader_desc.pixel_shaderはnullpterで良かった気がする
 	// テッセレーションしたい場合shader_desc.hull_shaderとshader_desc.domain_shaderのどちらも設定する必要あり
 	// 基本はshader_desc.vertex_shaderとshader_desc.pixel_shaderに値が設定されてればおけ
-	struct shader_desc 
+	struct shader_desc
 	{
 		ID3DBlob* vertex_shader = nullptr;
 		ID3DBlob* pixcel_shader = nullptr;
@@ -34,7 +34,7 @@ namespace dx12w
 	// ワイヤーフレームで描写できたり, ステンシルテストできたりとか
 	inline release_unique_ptr<ID3D12PipelineState> create_graphics_pipeline(ID3D12Device* device,
 		ID3D12RootSignature* rootSignature, std::vector<input_element> const& inputElements, std::vector<DXGI_FORMAT> const& renderTargetFormats,
-		shader_desc const& shaderDescs, bool depthEnable, bool alphaBlend, D3D12_PRIMITIVE_TOPOLOGY_TYPE primitiveTopologyType)
+		shader_desc const& shaderDescs, bool depthEnable, bool alphaBlend, D3D12_PRIMITIVE_TOPOLOGY_TYPE primitiveTopologyType);
 
 	// コンピュートシェーダを使う際のパイポラインの作成
 	inline release_unique_ptr<ID3D12PipelineState> create_compute_pipeline(ID3D12Device* device,
