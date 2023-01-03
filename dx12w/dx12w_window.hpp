66e70c4639daf9b49c96614c0d1394bf0a2ffe3a
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2023-01-03 13:19:47 +0900

    デフォルト引数の修正

--- dx12w/dx12w_window.hpp
+++ dx12w/dx12w_window.hpp
@@ -5,7 +5,7 @@ namespace dx12w
 {
 
 	// TODO: 32ビットの環境だとwindow_nameの型がcharでなければ行けないので対応する必要あり
-	inline HWND create_window(wchar_t const* window_name, LONG width, LONG height);
+	inline HWND create_window(wchar_t const* window_name, LONG width, LONG height, WNDPROC wnd_proc = WndProc);
 
 	// ウィンドウについてのメッセージを処理し更新する
 	// ウィンドウが閉じられた場合はfalseを返す. 通常はtrueを返す
