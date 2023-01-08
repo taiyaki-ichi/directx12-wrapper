3cbcb86ac61598beba6fa9e36f3bd94be7c4a52a
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2023-01-08 14:26:27 +0900

    不要な関数の削除

--- dx12w/dx12w_utility.hpp
+++ dx12w/dx12w_utility.hpp
@@ -49,33 +49,4 @@ namespace dx12w
 			return size + alignment - size % alignment;
 	}
 
-
-	// ñﬂÇËílÇÕsrcÅAdstÇÃèá
-	inline std::pair<D3D12_TEXTURE_COPY_LOCATION, D3D12_TEXTURE_COPY_LOCATION> get_texture_copy_location(
-		ID3D12Device* device, ID3D12Resource* srcResource, ID3D12Resource* dstResource)
-	{
-		D3D12_TEXTURE_COPY_LOCATION srcLocation{};
-		D3D12_TEXTURE_COPY_LOCATION dstLocation{};
-
-		auto const dstDesc = dstResource->GetDesc();
-
-		srcLocation.pResource = srcResource;
-		srcLocation.Type = D3D12_TEXTURE_COPY_TYPE_PLACED_FOOTPRINT;
-		{
-			D3D12_PLACED_SUBRESOURCE_FOOTPRINT footprint{};
-			UINT nrow;
-			UINT64 rowsize, size;
-			device->GetCopyableFootprints(&dstDesc, 0, 1, 0, &footprint, &nrow, &rowsize, &size);
-			srcLocation.PlacedFootprint = footprint;
-		}
-		srcLocation.PlacedFootprint.Offset = 0;
-
-
-		dstLocation.pResource = dstResource;
-		dstLocation.Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
-		dstLocation.SubresourceIndex = 0;
-
-		return { std::move(srcLocation),std::move(dstLocation) };
-	}
-
 }
